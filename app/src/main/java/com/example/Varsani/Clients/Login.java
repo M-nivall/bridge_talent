aae20e9c3039940a8c9bfdb591ea555d8a6bc293
nival <nivahmichael@gmail.com>
2025-12-29 17:48:28 +0300

    updated login

--- app/src/main/java/com/example/Varsani/Clients/Login.java
+++ app/src/main/java/com/example/Varsani/Clients/Login.java
@@ -214,4 +214,5 @@ public class Login extends AppCompatActivity {
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
     }
+
 }
