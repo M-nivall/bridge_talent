9fbf24ce96674a509334b631bd64a8eb37058a75
nival <nivahmichael@gmail.com>
2026-01-02 18:16:27 +0300

    implemnted auth

--- app/src/main/java/com/example/Varsani/Staff/StaffLogin.java
+++ app/src/main/java/com/example/Varsani/Staff/StaffLogin.java
@@ -65,7 +65,6 @@ public class StaffLogin extends AppCompatActivity {
 
         edt_select.setFocusable(false);
 
-
         session=new SessionHandler(getApplicationContext());
         user=session.getUserDetails();
 
