1a08af7d8e5c5c8bc58471382e653fef6feebde6
nival <nivahmichael@gmail.com>
2026-01-24 19:34:45 +0300

    added cart managemnt model

--- app/src/main/java/com/example/Varsani/Clients/Models/CartModal.java
+++ app/src/main/java/com/example/Varsani/Clients/Models/CartModal.java
@@ -12,6 +12,7 @@ public class CartModal {
     private String subToatl;
     private String stock;
 
+
     public CartModal(String productID, String productName, String quantity, String price,
                      String imgUrl, String itemID ,String subToatl,String stock) {
         this.productID = productID;
