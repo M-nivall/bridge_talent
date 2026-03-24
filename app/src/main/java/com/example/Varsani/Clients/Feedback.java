23ba982677eb2badac5c51aa1898b7c64c150d95
nivall <nivahmichael@gmail.com>
2026-03-24 22:56:21 +0300

    implemenented feedback

--- app/src/main/java/com/example/Varsani/Clients/Feedback.java
+++ app/src/main/java/com/example/Varsani/Clients/Feedback.java
@@ -225,5 +225,4 @@ public class Feedback extends AppCompatActivity {
         RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
         requestQueue.add(stringRequest);
     }
-
 }
