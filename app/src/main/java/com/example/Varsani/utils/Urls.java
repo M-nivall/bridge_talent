bbf401b530880dd705d77fe4d166595925573858
nival <nivahmichael@gmail.com>
2026-04-14 21:07:36 +0300

    updated url

--- app/src/main/java/com/example/Varsani/utils/Urls.java
+++ app/src/main/java/com/example/Varsani/utils/Urls.java
@@ -2,8 +2,8 @@ package com.example.Varsani.utils;
 
 public class Urls {
 
-    //public static String ipAddress = "http://192.168.170.29/bridge_talent/";
-    public static String ipAddress = "https://e5cb-197-248-201-21.ngrok-free.app/bridge_talent/";
+    public static String ipAddress = "http://192.168.137.140/bridge_talent/";
+    //public static String ipAddress = "https://e5cb-197-248-201-21.ngrok-free.app/bridge_talent/";
 
     private static final String ROOT_URL =ipAddress+ "android_files/";
     public static final String ROOT_URL_IMAGES =ipAddress+"upload_products/" ;
