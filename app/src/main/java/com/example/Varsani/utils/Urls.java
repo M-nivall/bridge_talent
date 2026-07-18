012c030b9b78abef78e014ad5d7f958a4cbad63b
nival <nivahmichael@gmail.com>
2026-07-18 23:27:50 +0300

    updated url

--- app/src/main/java/com/example/Varsani/utils/Urls.java
+++ app/src/main/java/com/example/Varsani/utils/Urls.java
@@ -2,7 +2,7 @@ package com.example.Varsani.utils;
 
 public class Urls {
 
-    //public static String ipAddress = "http://192.168.17.45/bridge_talent/";
+    //public static String ipAddress = "http://192.168.19.45/bridge_talent/";
     public static String ipAddress = "https://bridge-talent-admin-panel.up.railway.app/";
 
     private static final String ROOT_URL =ipAddress+ "android_files/";
